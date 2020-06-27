72e5f968f6c7abb7b03b4fb97ec500bb1c1efb80
sachin064 <sachinbgsachin3@gmail.com>
2020-06-27 23:59:37 +0530

    impemented date filter

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 import json
 from dateutil.parser import parse
-from flask import Flask, request,make_response, Response
+from flask import Flask, request, make_response, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from newsapi import NewsApiClient
 import requests
+from sqlalchemy import and_
 
 app = Flask(__name__)
 news_api = NewsApiClient(api_key="25e1b68d930346689d93ca5c9d6498e1")
@@ -25,7 +26,7 @@ class News_Data(db.Model):
     author = db.Column(db.String(100))
     title = db.Column(db.String(200))
     description = db.Column(db.String(500))
-    category = db.Column(db.String, db.ForeignKey(Category.name),nullable=False)
+    category = db.Column(db.String, db.ForeignKey(Category.name), nullable=False)
     published_at = db.Column(db.DateTime, nullable=False)
     url_image = db.Column(db.String(400))
 
@@ -48,7 +49,7 @@ def query_test():
                 'description': i.get('description'),
                 'published_at': dt.date(),
                 'url_image': i.get('url'),
-                'category':category
+                'category': category
             })
             news = News_Data(**data)
             db.session.add(news)
@@ -70,23 +71,23 @@ def category_post():
 
 @app.route('/api/list-news', methods=['POST'])
 def new_list():
-    categories=(request.args.get('category').split(','))
+    categories = (request.args.get('category').split(','))
+    date_from = request.args.get('from_date')
+    to_date = request.args.get('to_date')
     data = []
     for catego in list(categories):
-        new_cat = News_Data.query.filter_by(category=catego).all()
-        for i in new_cat:
-            l=({
+        new_data = db.session.query(News_Data).filter(and_(News_Data.published_at.between(date_from,to_date),News_Data.category.like(catego)))
+        for i in new_data:
+            l = ({
                 'author': i.author,
                 'title': i.title,
                 'description': i.description,
-                'published_at':i.published_at,
+                'published_at': i.published_at,
                 'url_image': i.url_image,
                 'category': i.category
             })
             data.append(l)
-    return make_response({'message':data})
-
-
+    return make_response({'message': data})
 
 
 if __name__ == '__main__':
